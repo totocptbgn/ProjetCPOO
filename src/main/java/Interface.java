06f794321e4d3769a95cdcdf880ecfdafeabe254
Thomas Copt-Bignon <lowx.add@gmail.com>
2019-12-21 15:56:16 +0100

    version 1.6.1

--- src/main/java/Interface.java
+++ src/main/java/Interface.java
@@ -2,8 +2,10 @@ import javax.print.attribute.standard.NumberUp;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Path;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * Classe gérant l'interface du gestionnaire
@@ -29,9 +31,9 @@ public class Interface {
 		Scanner sc = new Scanner(System.in);
 		clearTerminal();
 		ColoredOutput.init();
-		printHeader();
 		gstn = new Gestionnaire();
-
+		printHeader();
+		
 		while (running) {
 			System.out.print("> ");
 			String cmd = sc.nextLine();
@@ -52,20 +54,19 @@ public class Interface {
 	private static void newCommand(String cmd) throws IOException {
 
 		// Ne fais rien quand rien n'est tapé
-		if (cmd.matches("^\\p{Blank}*$"))
-		{
+		if (cmd.matches("^\\p{Blank}*$")) {
 			return;
 		}
 
 		// Ferme le programme
-		if (cmd.matches("^exit\\p{Blank}*")) {
+		if (cmd.matches("\\p{Blank}*exit\\p{Blank}*")) {
 			exit();
 			clearTerminal();
 			return;
 		}
 
 		// Clear le terminal
-		if (cmd.matches("^clear\\p{Blank}*")) {
+		if (cmd.matches("\\p{Blank}*clear\\p{Blank}*")) {
 			clearTerminal();
 			return;
 		}
@@ -74,13 +75,12 @@ public class Interface {
 		if (cmd.matches("^add .+")) {
 			String link = cmd.substring(4);
 
-			if (!link.matches("(http(s)?:\\/\\/.)(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\/([0-9a-zA-Z./])+")) {
-				System.out.println(ColoredOutput.set(Color.RED, "'" + link + "'" + " is not a correct link."));
+			if (!link.matches("(http(s)?:\\/\\/.)(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\/([*0-9a-zA-Z._\\-/=])+")) {
+				System.out.println("Error: This is not a correct link.");
 				return;
 			}
-			System.out.println(ColoredOutput.set(Color.GREEN, "'" + link + "'" + " is a correct link."));
 			gstn.addLauncher(link);
-			System.out.println(gstn.getCurrentNew().getNom() + " was added with id : " + gstn.getCurrentNew().getId());
+			System.out.println(gstn.getCurrentNew().getNom() + " was added with id : [" + gstn.getCurrentNew().getId() + "]");
 			return;
 		}
 
@@ -92,14 +92,11 @@ public class Interface {
 		}
 
 		// Launch le dernier Laucher ajouté
-		if (cmd.matches("^launch")) {
+		if (cmd.matches("\\p{Blank}*launch\\p{Blank}*")) {
 			Launcher t;
 			try {
 				t = gstn.getCurrentNew();
-				Map<Path,String> pages = t.getPages();
-				for (Map.Entry<Path, String> entry : pages.entrySet()) {
-					System.out.println(entry.getKey() + "  | " + entry.getValue());
-				}
+				System.out.println("Launching [" + t.getId() + "]");
 				gstn.launch();
 			} catch (NullPointerException n) {
 				System.out.println("Error: there is no launcher to start.");
@@ -109,8 +106,14 @@ public class Interface {
 		}
 
 		// Commandes mal utilisées
-		if (cmd.matches("^add\\p{Blank}*")) {
+		if (cmd.matches("\\p{Blank}*add\\p{Blank}*")) {
 			System.out.println(ColoredOutput.set(Color.RED, "Usage: add [link]"));
+			return;
+		}
+
+		if (cmd.matches("\\p{Blank}*list\\p{Blank}*")) {
+			Iterator<Launcher> it = gstn.listOfAll().iterator();
+			printListOfLauncher(it);
 			return;
 		}
 
@@ -124,30 +127,32 @@ public class Interface {
 	}
 
 	// Affiche l'en-tête du programme
-	private static void printHeader() {
+	private static void printHeader() throws IOException {
 		System.out.print(
-				" + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +\n" +
-				" |                                                                                                       |\n" +
-				" |                                                                                                       |\n" +
-				" |               ██████╗  ██████╗ ██╗    ██╗███╗   ██╗██╗      ██████╗  █████╗ ██████╗                   |\n" +
-				" |               ██╔══██╗██╔═══██╗██║    ██║████╗  ██║██║     ██╔═══██╗██╔══██╗██╔══██╗                  |\n" +
-				" |               ██║  ██║██║   ██║██║ █╗ ██║██╔██╗ ██║██║     ██║   ██║███████║██║  ██║                  |\n" +
-				" |               ██║  ██║██║   ██║██║███╗██║██║╚██╗██║██║     ██║   ██║██╔══██║██║  ██║                  |\n" +
-				" |               ██████╔╝╚██████╔╝╚███╔███╔╝██║ ╚████║███████╗╚██████╔╝██║  ██║██████╔╝                  |\n" +
-				" |               ╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝                   |\n" +
-				" |                                                                                                       |\n" +
-				" |                     ███╗   ███╗ █████╗ ███╗   ██╗ █████╗  ██████╗ ███████╗██████╗                     |\n" +
-				" |                     ████╗ ████║██╔══██╗████╗  ██║██╔══██╗██╔════╝ ██╔════╝██╔══██╗                    |\n" +
-				" |                     ██╔████╔██║███████║██╔██╗ ██║███████║██║  ███╗█████╗  ██████╔╝                    |\n" +
-				" |                     ██║╚██╔╝██║██╔══██║██║╚██╗██║██╔══██║██║   ██║██╔══╝  ██╔══██╗                    |\n" +
-				" |                     ██║ ╚═╝ ██║██║  ██║██║ ╚████║██║  ██║╚██████╔╝███████╗██║  ██║                    |\n" +
-				" |                     ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝                    |\n" +
-				" |                                                                                                       |\n" +
-				" |                                                    *    Dao Thauvin & Thomas Copt-Bignon     *        |\n" +
-				" |                                                    *             version 1.0.0               *        |\n" +
-				" |                                                    *  CPOO | Final project | year 2019-2020  *        |\n" +
-				" |                                                                                                       |\n" +
-				" + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +\n\n"
+				" + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +\n" +
+						" |                                                                                                                         |\n" +
+						" |                                                                                                                         |\n" +
+						" |                        ██████╗  ██████╗ ██╗    ██╗███╗   ██╗██╗      ██████╗  █████╗ ██████╗                            |\n" +
+						" |                        ██╔══██╗██╔═══██╗██║    ██║████╗  ██║██║     ██╔═══██╗██╔══██╗██╔══██╗                           |\n" +
+						" |                        ██║  ██║██║   ██║██║ █╗ ██║██╔██╗ ██║██║     ██║   ██║███████║██║  ██║                           |\n" +
+						" |                        ██║  ██║██║   ██║██║███╗██║██║╚██╗██║██║     ██║   ██║██╔══██║██║  ██║                           |\n" +
+						" |                        ██████╔╝╚██████╔╝╚███╔███╔╝██║ ╚████║███████╗╚██████╔╝██║  ██║██████╔╝                           |\n" +
+						" |                        ╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝                            |\n" +
+						" |                                                                                                                         |\n" +
+						" |                            ███╗   ███╗ █████╗ ███╗   ██╗ █████╗  ██████╗ ███████╗██████╗                                |\n" +
+						" |                            ████╗ ████║██╔══██╗████╗  ██║██╔══██╗██╔════╝ ██╔════╝██╔══██╗                               |\n" +
+						" |                            ██╔████╔██║███████║██╔██╗ ██║███████║██║  ███╗█████╗  ██████╔╝                               |\n" +
+						" |                            ██║╚██╔╝██║██╔══██║██║╚██╗██║██╔══██║██║   ██║██╔══╝  ██╔══██╗                               |\n" +
+						" |                            ██║ ╚═╝ ██║██║  ██║██║ ╚████║██║  ██║╚██████╔╝███████╗██║  ██║                               |\n" +
+						" |                            ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝                               |\n" +
+						" |                                                                                                                         |\n" +
+						" |                                                                        *    Dao Thauvin & Thomas Copt-Bignon     *      |\n" +
+						" |                                                                        *             version 1.0.0               *      |\n" +
+						" |                                                                        *  CPOO | Final project | year 2019-2020  *      |\n" +
+						" |                                                                                                                         |\n" +
+						" + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +\n\n" +
+						"  Type 'help' to get details.\n" +
+						"  The download directory is at " + gstn.pathDownload() + "\n\n"
 		);
 	}
 
@@ -155,6 +160,40 @@ public class Interface {
 	private static void clearTerminal() {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
+	}
+
+	// Affiche les launcher avec leur id, état, nom et taille
+	private static void printListOfLauncher(Iterator<Launcher> it) {
+		while (it.hasNext()){
+			Launcher l = it.next();
+			System.out.println(l.getId() + " | " + l.getNom() + " | " + l.getEtat() + " | " + getSizeDownloaded(l));
+		}
+	}
+
+	// Renvoie un String avec le pourcentage d'avancement de download et la taille totale en octet
+	private static String getSizeDownloaded(Launcher l) {
+		long downloaded  = l.getTotalSize() - l.getSizeLeft();
+		long total = l.getTotalSize();
+		String sizeof = "% of ";
+		if (l.getTotalSize() == -1f) {
+			sizeof += "unknown size.";
+		} else {
+			sizeof+= humanReadableSize(l.getTotalSize());
+		}
+		return (downloaded * 100 / total) + sizeof;
+	}
+
+	// Renvoie un String représentant une taille en octet facilemnt lisible
+	private static String humanReadableSize(long bytes) {
+		String s = bytes < 0 ? "-" : "";
+		long b = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+		return b < 1000L ? bytes + " B"
+				: b < 999_950L ? String.format("%s%.1f kB", s, b / 1e3)
+				: (b /= 1000) < 999_950L ? String.format("%s%.1f MB", s, b / 1e3)
+				: (b /= 1000) < 999_950L ? String.format("%s%.1f GB", s, b / 1e3)
+				: (b /= 1000) < 999_950L ? String.format("%s%.1f TB", s, b / 1e3)
+				: (b /= 1000) < 999_950L ? String.format("%s%.1f PB", s, b / 1e3)
+				: String.format("%s%.1f EB", s, b / 1e6);
 	}
 
 	/**
